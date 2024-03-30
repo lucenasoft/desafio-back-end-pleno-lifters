8a2d8f1fb5cfc3df67191a1e28822e2da6fd9643
lucenasoft <getugbr@gmail.com>
2024-03-30 11:24:26 -0300

    started project

---
+++ src/test/java/com/desafio/urna/StartupTests.java
@@ -0,0 +1,13 @@
+package com.desafio.urna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
